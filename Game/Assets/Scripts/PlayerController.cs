0d86eb909bcbc682d8fa5a32b06e28735392077f
Will Norton <norton402@morrisville.edu>
2020-03-31 16:31:25 -0400

    Very simple turning mechanism added in player controller

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Range(1, 100)] [SerializeField] float sensitivityX = 10;
+    [Range(1, 100)] [SerializeField] float sensitivityY = 10;
 
+    float minimumY = -60F;
+    float maximumY = 60F;
+
+    float rotationY = 0F;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Vector3 lastMousePos = Input.mousePosition;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
     }
 }
