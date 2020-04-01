6eac48ec09c6fad7e7e8edffcde1361462814f85
Will Norton <norton402@morrisville.edu>
2020-04-01 15:21:50 -0400

    Started working on movement

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -19,10 +20,19 @@ public class PlayerController : MonoBehaviour
     //starts the player's x rotation at 0
     float rotationX = 0F;
 
+    //character controller used for moving 
+    CharacterController characterController;
+    //other variables used for moving
+    [SerializeField] float speed = 6f;
+    [SerializeField] float jumpSpeed = 8f;
+    [SerializeField] float gravity = 20f;
+    Vector3 moveDirection = Vector3.zero;
+
     private void Start()
     {
         LockCursor();
         Vector3 lastMousePos = Input.mousePosition;
+        characterController = GetComponent<CharacterController>();
     }
 
     private static void LockCursor()
@@ -34,22 +44,44 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
+        movement();
         updateRotation();
+    }
+
+    private void movement()
+    {
+        if (characterController.isGrounded) 
+        {
+            //recalculate move direction 
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        //apply gravity the first time 
+        //gravity is applied twice because it is an acceleration 
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        //move the controller 
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 
     private void updateRotation()
     {
         //calculate the y rotation of the head, this is used for the body also 
-        float rotationY = head.transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+        float rotationY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
 
         //calculate the x rotation
         rotationX += Input.GetAxis("Mouse Y") * sensitivityY;
         rotationX = Mathf.Clamp(rotationX, minimumY, maximumY);
 
 
-        //only rotate the body in the Y direction 
-        body.transform.localEulerAngles = new Vector3(0, rotationY, 0);
-        //rotate the head in both directions
-        head.transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
+        //rotate the body in the y direction, this also rotates the head in the y direction 
+        transform.localEulerAngles = new Vector3(0, rotationY, 0);
+        //rotate the head in the x direction
+        head.transform.localEulerAngles = new Vector3(-rotationX, 0, 0);
     }
 }
