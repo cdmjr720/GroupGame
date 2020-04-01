898653f117c91b3ee3ea7ee5e35af9e4a50173b0
Will Norton <norton402@morrisville.edu>
2020-04-01 13:17:37 -0400

    Better functionality for turning and looking

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -4,29 +4,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //player head and body 
+    [SerializeField] GameObject body;
+    [SerializeField] GameObject head;
+
+    //looking sensitivity 
     [Range(1, 100)] [SerializeField] float sensitivityX = 10;
     [Range(1, 100)] [SerializeField] float sensitivityY = 10;
 
+    //won't let you look straight up or straight down 
     float minimumY = -60F;
     float maximumY = 60F;
 
-    float rotationY = 0F;
+    //starts the player's x rotation at 0
+    float rotationX = 0F;
 
     private void Start()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        LockCursor();
         Vector3 lastMousePos = Input.mousePosition;
     }
-    // Update is called once per frame
+
+    private static void LockCursor()
+    {
+        //locks the cursor in the game so you can't see it 
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
     void Update()
     {
-        float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+        updateRotation();
+    }
 
-        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+    private void updateRotation()
+    {
+        //calculate the y rotation of the head, this is used for the body also 
+        float rotationY = head.transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
 
-        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        //calculate the x rotation
+        rotationX += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationX = Mathf.Clamp(rotationX, minimumY, maximumY);
 
+
+        //only rotate the body in the Y direction 
+        body.transform.localEulerAngles = new Vector3(0, rotationY, 0);
+        //rotate the head in both directions
+        head.transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
     }
 }
