870fbffd3f7ea2cdd68d17833cfc0ff035bbbb8c
cdmjr720 <charlesdmaitland@gmail.com>
2020-04-08 14:47:10 -0400

    Added capsule collider to native

--- Game/Assets/Scripts/AIController.cs
+++ Game/Assets/Scripts/AIController.cs
@@ -5,21 +5,24 @@ using UnityEngine.AI;
 
 public class AIController : MonoBehaviour
 {
-
+    //sets "chase distance" for enemy to find player
     [SerializeField] float chaseDistance = 3f;
+    //sets "target" for enemy to find player
     [SerializeField] Transform target;
 
     private void Update()
     {
+        //if player is within chase distance, sets enemy to move towards them
         if (DistanceToPlayer() < chaseDistance)
         {
-            //Debug.Log("Should Chase");
             GetComponent<NavMeshAgent>().destination = target.position;
         }
     }
 
+    //float created for DistanceToPlayer
     private float DistanceToPlayer()
     {
+        //finds player with "Player" tag
         GameObject player = GameObject.FindWithTag("Player");
         return Vector3.Distance(player.transform.position, transform.position);
     }
