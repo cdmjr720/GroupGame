f4f50e491f3fafba1e55fd27e5520c39f8d997d2
Will Norton <norton402@morrisville.edu>
2020-04-20 15:54:37 -0400

    Started work for collectable rocks

---
+++ Game/Assets/Scripts/Rock.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    //item picked up 
+    [SerializeField] Item rockItem;
+    //players' inventory 
+    InventorySystem inventorySystem;
+
+    //can be hit 2 times 
+    private int timesHit = 0;
+    private int maxTimesHit = 2;
+
+    private void Start()
+    {
+        inventorySystem = FindObjectOfType<InventorySystem>();
+    }
+
+    public void Interact()
+    {
+        if (timesHit < maxTimesHit - 1)
+        {
+            inventorySystem.AddItem(rockItem, 1);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        //This will update the player controllers list of interactable objects 
+    }
+}
