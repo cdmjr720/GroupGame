88c95b3246043854876e9191a945668a7880b695
cdmjr720 <charlesdmaitland@gmail.com>
2020-04-20 15:24:34 -0400

    working on "fishing" mechanism

--- Game/Assets/Scripts/Fishing.cs
+++ Game/Assets/Scripts/Fishing.cs
@@ -6,17 +6,39 @@ using UnityEngine;
 
 public class Fishing : MonoBehaviour
 {
+    //variable and bool for destination target (so fish "swims")
     public Transform[] target;
     bool isMoving = false;
-
+    //creates new target to keep fish moving
+    private Transform newTarget;
+    //fish speed defined as 5
     float speed = 5.0f;
 
-    private Transform newTarget;
+    //textures for the fish and for when fish is caught
+    [SerializeField] GameObject fish;
+    [SerializeField] Item fishItem;
+    GameObject fishInstance;
+
+    //allows for inventory system to be called
+    InventorySystem inventorySystem;
+
+    //relates to the difficulty of catching fish
+    [SerializeField] [Range(1, 10)] float difficulty = 9f;
+
+
+    private void Start()
+    {
+        //calls inventory system
+        inventorySystem = FindObjectOfType<InventorySystem>();
+
+    }
 
     public void Update()
     {
+        //code for making fish "swim"
         if (isMoving == false)
         {
+            //if fish is not moving, makes fish move to random target
             newTarget = target[Random.Range(0, target.Length)];
             isMoving = true;
         }
@@ -28,9 +50,12 @@ public class Fishing : MonoBehaviour
             isMoving = false;
         }
 
-
-
     }
 
+    public void Catch()
+    {
+        Destroy(fishInstance);
+        inventorySystem.AddItem(fishItem, 1);
+    }
 
 }
