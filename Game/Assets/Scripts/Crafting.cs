833ae2f2900d43191a55cc7fa0dfdb8456d45b32
Will Norton <norton402@morrisville.edu>
2020-04-24 12:20:35 -0400

    Added functionality for crafting sword

---
+++ Game/Assets/Scripts/Crafting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crafting : MonoBehaviour
+{
+    //This script is used for crafting shelter and weapons
+
+    //items used for crafting 
+    [SerializeField] Item woodItem;
+    [SerializeField] Item rockItem;
+
+    //crafted items 
+    [SerializeField] Item swordItem;
+
+    //inventory of player 
+    InventorySystem inventory;
+
+    private void Start()
+    {
+        inventory = FindObjectOfType<InventorySystem>();
+    }
+
+    public void CraftSword()
+    {
+        //TODO check for empty slot
+
+        //sword requires one wood and two rock 
+        if (inventory.RemoveItem(woodItem, 1) && inventory.RemoveItem(rockItem, 2))
+        {
+            inventory.AddItem(swordItem, 1);
+        }
+    }
+}
