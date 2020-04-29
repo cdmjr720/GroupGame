4ac877ca6e4821ed464ddea5484e8ab4628f6be8
Will Norton <norton402@morrisville.edu>
2020-04-29 15:10:11 -0400

    Added hunger bar that slowly decreases

---
+++ Game/Assets/Scripts/Hunger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunger : MonoBehaviour
+{
+    [SerializeField] float hunger = 100f;
+    [SerializeField] RectTransform foreground = null;
+
+    [SerializeField] float timeBetweenHungerIncrease = 10f;
+
+    private float timeSinceHungerIncrease;
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreground.localScale = new Vector3(GetFraction(), 1, 1);
+        if (timeSinceHungerIncrease > timeBetweenHungerIncrease)
+        {
+            //lower hunger bar
+            hunger -= 5;
+            timeSinceHungerIncrease = 0;
+        }
+        timeSinceHungerIncrease += Time.deltaTime;
+    }
+
+    public float GetFraction()
+    {
+        return hunger / 100;
+    }
+}
